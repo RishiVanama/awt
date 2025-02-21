84af8faf1ab1112572407c43d09e2e54227216f1
RishiVanama <vanamarishi@gmail.com>
2025-02-22 02:16:53 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,6 +1,6 @@
 const fetchUser = (id,callback)=>{
     console.log("Fetching user name");
-    callback("varun");
+    callback("Rishi");
 }
 const fetchOrder = (username,callback) =>{
     console.log(`Fetching ${username} order`);
